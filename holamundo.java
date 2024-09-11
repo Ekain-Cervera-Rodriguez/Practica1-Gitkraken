86595113e9e5d65a9c2ac4461dc569bd4ca9a7c3
Ekain Cervera <ekainisraelcerverarodriguez@gmail.com>
2024-09-11 12:09:37 -0600

    Cambio de iterador

--- holamundo.java
+++ holamundo.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String[] textos = {"hola mundo!"};
-        for (int k = 0; k < textos.length; k++) {
-            String textoMayus = textos[k].toUpperCase();
+        for (int i = 0; i < textos.length; i++) {
+            String textoMayus = textos[i].toUpperCase();
             System.out.println(textoMayus);
         }
     }
